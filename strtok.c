0cadaab57f27802cd22ed8265ea19c15525ab10b
Peter.YE <peter.ye.datawind@gmail.com>
2015-03-31 09:45:27 -0400

    config_file_reading_bad_code

--- strtok.c
+++ strtok.c
@@ -1,21 +1,37 @@
 #include <string.h>
 #include <stdio.h>
 
+#define CONFIG_FILE_NAME "config.txt"
+#define CONFIG_FILE_LINE_MAX  80
+#define CONFIG_FILE_DELIMITER "="
+
 int main()
 {
    const char str[80] = "This is - www.tutorialspoint.com - website";
    const char s[2] = "-";
-   char *token;
+   char *p_name, *p_value;
+
+
+   FILE *config_file;
+   char line[CONFIG_FILE_LINE_MAX];
+
+   config_file =  fopen(CONFIG_FILE_NAME, "r+");
+
+   //fget(line, );
+   fgets(line,CONFIG_FILE_LINE_MAX, config_file );
+
    
    /* get the first token */
-   token = strtok(str, s);
+   p_name = strtok(line, CONFIG_FILE_DELIMITER);
    
    /* walk through other tokens */
-   while( token != NULL ) 
+   while( p_name != NULL ) 
    {
-      printf( " %s\n", token );
     
-      token = strtok(NULL, s);
+      p_value = strtok(NULL, CONFIG_FILE_DELIMITER);
+      printf( " name == %s,  value == %s \n", p_name, p_value );
+   	 fgets(line,CONFIG_FILE_LINE_MAX, config_file );
+     p_name = strtok(line, CONFIG_FILE_DELIMITER);
    }
    
    return(0);
