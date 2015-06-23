c8c77872a9db73f230211dd245bbfb6950f19400
Peter.YE <peter.ye.datawind@gmail.com>
2015-06-23 19:48:26 -0400

    error on the cast

---
+++ short_on_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char array[10] = {1,2,3,4,5,252,0 };
+	int size, i;
+	short cast_short;
+	short *p_cast_short;
+	size = sizeof(array);
+
+	printf("\n");
+	for(i=0; i< size; i++){
+		printf(" %d", array[i]);
+	}
+	printf("\n");
+
+		p_cast_short =(short *) &(array[5]);
+		cast_short = * p_cast_short;
+
+		printf("%d", cast_short);
+		printf("\n");
+
+		cast_short = (short) array[5];
+		printf("%d", cast_short);
+		printf("\n");
+
+}
