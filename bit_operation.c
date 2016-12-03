f900360904df263770e2f373210356c14e37c414
Peisheng.YE <peisheng.ye.88@gmail.com>
2016-12-02 19:09:04 -0500

    swap consecutive bits

--- bit_operation.c
+++ bit_operation.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+/*
+ * good bits operation in the following link
+http://graphics.stanford.edu/~seander/bithacks.html#SwappingBitsXOR
+*/
+
 void print_out_bits(unsigned int input);
 unsigned int swapBits(unsigned int input);
 
-int main(){
-    unsigned int input = 0x55U;
-    unsigned int output;
-
-    print_out_bits(input);
-
-    output = swapBits(input);
-
-    print_out_bits(output);
-
-
-}
 
 unsigned int swapBits(unsigned int input){
     int tmp_odd, tmp_event, ret;
@@ -42,4 +37,34 @@ void print_out_bits(unsigned int input){
     }
     printf("\n");
 }
+
+
+void swapIndividualBitsWithXOR( unsigned int input, unsigned int position_i,
+        unsigned int position_j ){
+
+unsigned int i, j; // positions of bit sequences to swap
+unsigned int n;    // number of consecutive bits in each sequence
+unsigned int ret;    
+
+unsigned int x = ((input >> position_i) ^ (input >> position_j)) & ((1U << n) - 1); // XOR temporary
+ret = input ^ ((x << i) | (x << j));
+return ret;
+    
+}
+
+
+int main(){
+    unsigned int input = 0x55U;
+    unsigned int output;
+
+    print_out_bits(input);
+
+    output = swapBits(input);
+
+    print_out_bits(output);
+
+
+}
+
+
 
